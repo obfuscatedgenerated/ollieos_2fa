9a9ee5a2f1bca509fcab3c88433419d2463db756
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-03-07 00:52:45 +0000

    remove old comment

--- src/index.ts
+++ src/index.ts
@@ -82,9 +82,6 @@ const truncate_hotp = (hash: Uint8Array) => {
         (hash[offset + 3] & 0xff)
     );
 
-    // TODO: this is broken with certain keys?
-    // e.g. from test site I65VU7K5ZQL7WB4E gave 7775 at 00:44 07/03/2025
-
     // truncate to 6 digits
     return value % (10 ** TOTP_DIGITS);
 }
